6ca14419ee745b36bd325a5be377bdb5d7c0165a
xxxROXIxxx <roxi@gmail.com>
2025-06-25 14:32:08 +0300

    Changes to be committed: 	modified:   cfg.py

--- cfg.py
+++ cfg.py
@@ -1,3 +1,3 @@
-hotkey = ["alt","ctrl","shift","esc","win","alt"and"F4"]
-answNo = ["нет","no"]
-answYes = ["да","yes"]
+HOTKEY_LIST = ["alt","ctrl","shift","esc","win","alt"and"F4"]
+ANSW_NO_LIST = ["нет","no"]
+ANSW_YES_LIST = ["да","yes"]
