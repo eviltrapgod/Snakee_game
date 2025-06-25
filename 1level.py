aeddb8e1396870a498458ee74479b4ce9fa3a592
xxxROXIxxx <roxi@gmail.com>
2025-06-25 14:09:51 +0300

    Changes to be committed: 	deleted:    1 level.py 	new file:   1level.py 	new file:   confg.py 	modified:   tet.py

---
+++ 1level.py
@@ -0,0 +1,14 @@
+import subprocess
+import webbrowser
+import keyboard
+import time
+import os
+import random
+import win32gui
+from win32api import GetSystemMetrics
+from tet import lvl
+
+if lvl == 1:
+        while lvl1:
+                keyboard.block_key("alt","ctrl","shift","esc","win","alt"and"F4")
+                webbrowser.open("https://www.youtube.com/watch?v=xvFZjo5PgG0")
---
+++ confg.py
@@ -0,0 +1,3 @@
+hotkey = ["alt","ctrl","shift","esc","win","alt"and"F4"]
+otvetnet = ["нет","no"]
+otvetda = ["да","yes"]
