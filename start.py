851d091c26028afef071dc7deb05a406f0f1b974
trapgod <null@gmail.com>
2025-06-26 22:01:44 +0300

    Изменения, которые будут включены в коммит: 	изменено:      cfg.py 	изменено:      start.py

--- start.py
+++ start.py
@@ -1,19 +1,20 @@
 import cfg
 
 # функция выбора не постоянных параметров и приветствие
-def choose_game_settings():
+
+def print_start_background():
     print(cfg.START_BACKGROUND)
+
+def choose_game_settings():
     # Функция выбора имени пользователя
     def choose_username():
         username = input("Введите ваше имя: ")
         start_msg = f"{username}, Добро пожаловать в игру Snake! Или.."
         return start_msg
     # Запись стартового сообщения в переменную
-    start_msg = choose_username()
-
+    start_msg = choose_username()    
     # Выбор уровня сложности и приветствие
-    def choose_difficulty(start_msg):
-        print(start_msg)
+    def choose_difficulty():
         print("""Выберите уровень сложности:
               1. Легкий
               2. Средний
@@ -28,7 +29,7 @@ def choose_game_settings():
 
         except ValueError:
             print("Некорректный ввод. Попробуйте еще раз.")
-            return choose_difficulty(start_msg)
+            return choose_difficulty()
     # запись в переменную выбранный уровень сложности
-    choosed_game_difficulty = choose_difficulty(start_msg)
+    choosed_game_difficulty = choose_difficulty()
 
