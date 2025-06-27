74313fd1f460de8862c580e3c4dee67e485ff72d
trapgod <null@gmail.com>
2025-06-27 20:31:15 +0300

    Текущая ветка: main Эта ветка соответствует «origin/main».
    Изменения, которые будут включены в коммит:
    	изменено:      main.py

--- main.py
+++ main.py
@@ -2,20 +2,25 @@ import start, redirect, check_agreement
 import sys
 # вызов функциональной части программы
 def main(username, game_difficulty):
-    # проверка статуса соглашения с условиями пользования
-    if check_agreement.check_agreement_status() == False:
-        sys.exit()
-    elif check_agreement.check_agreement_status() == True:
-        # вывод стартового экрана
-        start.print_start_background()
-        # выбор имени пользователя
-        username = start.choose_username()
-        # вывод приветственного сообщения
-        start.print_hello_message(username)
-        # выбор уровня сложности
-        game_difficulty = start.choose_difficulty()
-        # перенаправление в игру с выбранным уровнем сложности
-        redirect.redirect_to_level(game_difficulty)
+    # попытка выполнения программы 
+    try:
+        # проверка статуса соглашения с условиями пользования
+        if check_agreement.check_agreement_status() == False:
+            sys.exit()
+        elif check_agreement.check_agreement_status() == True:
+            # вывод стартового экрана
+            start.print_start_background()
+            # выбор имени пользователя
+            username = start.choose_username()
+            # вывод приветственного сообщения
+            start.print_hello_message(username)
+            # выбор уровня сложности
+            game_difficulty = start.choose_difficulty()
+            # перенаправление в игру с выбранным уровнем сложности
+            redirect.redirect_to_level(game_difficulty)
+    # обработка непредвиденных ошибок
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
 
 # вход в программу
 if __name__ == "__main__":
