f88b8e8725fd34f0184728c9862e5b8e14b2ab9c
trapgod <null@gmail.com>
2025-06-27 20:16:26 +0300

    Full work check agreement module and small fix

--- check_agreement.py
+++ check_agreement.py
@@ -1,3 +1,18 @@
+import cfg
+
 def check_agreement_status():
-    # Здесь будет логика проверки соглашения
-    pass
+    print(cfg.const.agreement_text)
+    try:
+        user_agreement = input("Вы согласны с условиями? (да/нет): ")
+        if user_agreement.lower() in cfg.const.ANSW_YES_LIST:
+            return True
+        elif user_agreement.lower() in cfg.const.ANSW_NO_LIST:
+            return False
+        else:
+            raise ValueError
+    except ValueError:
+        print("Некорректный ввод. Пожалуйста, ответьте 'да' или 'нет'.")
+        return check_agreement_status()
+    except Exception as e:
+        print(f"Произошла ошибка: {e}")
+        return False
--- main.py
+++ main.py
@@ -1,19 +1,21 @@
 import start, redirect, check_agreement
-
+import sys
 # вызов функциональной части программы
 def main(username, game_difficulty):
-    # вывод стартового экрана
-    start.print_start_background()
     # проверка статуса соглашения с условиями пользования
-    check_agreement.check_agreement_status()
-    # выбор имени пользователя
-    username = start.choose_username()
-    # вывод приветственного сообщения
-    start.print_hello_message(username)
-    # выбор уровня сложности
-    game_difficulty = start.choose_difficulty()
-    # перенаправление в игру с выбранным уровнем сложности
-    redirect.redirect_to_level(game_difficulty)
+    if check_agreement.check_agreement_status() == False:
+        sys.exit()
+    elif check_agreement.check_agreement_status() == True:
+        # вывод стартового экрана
+        start.print_start_background()
+        # выбор имени пользователя
+        username = start.choose_username()
+        # вывод приветственного сообщения
+        start.print_hello_message(username)
+        # выбор уровня сложности
+        game_difficulty = start.choose_difficulty()
+        # перенаправление в игру с выбранным уровнем сложности
+        redirect.redirect_to_level(game_difficulty)
 
 # вход в программу
 if __name__ == "__main__":
